83ab156db0208fd8b7ed4a69271ce112858c9e7b
Alex McGuigan <amcguiga@gmail.com>
2022-01-16 14:22:25 -0500

    removing the cookies, I don't actually need them

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ import { parsePage as castlightParsePage } from './src/castlight-health-testing-
 import { parsePage as nycGovParsePage } from './src/nyc-gov-covid-testing-page/parse-page.js'
 import { loadPage as castlightLoad } from './src/castlight-health-testing-sites/load-page.js'
 import { loadPage as nycGovLoad } from './src/nyc-gov-covid-testing-page/load-page.js'
-import { parseFile } from './src/parse-file.js'
 import { writeFile } from './src/write-file.js'
-
-const privateSitePath = new URL('./data/site-references/private-covid-sites-1-13-22.html', import.meta.url).pathname
-const govSitePath = new URL('./data/site-references/city-covid-sites-1-13-22.html', import.meta.url).pathname
 
 const init = (async () => {
   let privateData = castlightLoad().then(page => 
